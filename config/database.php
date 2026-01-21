3dc789022a95307c33f8c4e65b7e9f89bfb7bf01
Your Name <you@example.com>
2026-01-21 14:34:29 +0000

    database connection

--- config/database.php
+++ config/database.php
@@ -2,16 +2,29 @@
 
 // Database connection details
 $host = 'localhost';
-$username = 'root';
-$password = '';
+$username = 'karen';
+$password = 'password';
 $database = 'fastburgers';
 
-// Attempt to create a database connection
+/**
+ * Create a new mysqli connection
+ */
 $conn = new mysqli($host, $username, $password, $database);
 
+/**
+ * Check for connection errors
+ */
+if ($conn->connect_error) {
+    die('Database connection failed: ' . $conn->connect_error);
+}
+else echo('connection successful');
 
-// Set the character set after a successful connection
+/**
+ * Set the character set (important for security and emojis)
+ */
 $conn->set_charset('utf8mb4');
 
-// Return the connection for use in controllers
+/**
+ * Return the connection so it can be used elsewhere
+ */
 return $conn;
